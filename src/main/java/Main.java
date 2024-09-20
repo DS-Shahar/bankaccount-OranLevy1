acde7ecb45f5cf44a99ca52b30e4001a76bbe843
OranLevy1 <oranlevy9@gmail.com>
2024-09-20 16:53:32 +0300

    Add files via upload

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -1,25 +1,28 @@
+import java.io.Reader;
+import java.util.Scanner;
+
 public class Main {
+	public static Scanner reader= new Scanner (System.in);
 	public static void main(String[] args) {
-	/*BankAccount acct1 = new BankAccount(3000, -1000);
-        System.out.println(acct1.getBalance());
-        boolean success = acct1.withdraw(1500);
-        System.out.println("success=" + success + ", balance=" + acct1.getBalance());
-        acct1.deposit(500);
-        System.out.println("balance=" + acct1.getBalance());
-        BankAccount acct2 = new BankAccount(3000, -1000);
-        boolean success = acct1.transfer(acct2, 500);
-        System.out.println("transfer success = " + success);
-        System.out.println("balance=" + acct2.getBalance());*/
-	BankAccount Lisa = new BankAccount(1000,-1000,"Lisa");
-	BankAccount Bob = new BankAccount(1000,-1000,"Bob");
-	System.out.println(Lisa.toString());
-	System.out.println(Bob.toString());
-	boolean success = Lisa.transfer(Bob, 500);
-	System.out.println(Lisa.toString());
-	System.out.println(Bob.toString());
-        System.out.println("transfer success = " + success);
-        success = Lisa.transfer(Bob, 1600);
-        System.out.println("transfer success = " + success);
+		int len1 = reader.nextInt();
+		int wid1 = reader.nextInt();
+		int len2 = reader.nextInt();
+		int wid2 = reader.nextInt();
+		Rectangle rec1 = new Rectangle (len1,wid1);
+		Rectangle rec2 = new Rectangle (len2,wid2);
+		System.out.println(rec1.toString() + "\n" + rec2.toString());
+		rec1.draw();
+		rec2.draw();
+		rec1.scale(2);
+		System.out.println("new rectangle : "); 
+		rec1.draw();
+		int rec1_area= rec1.calcArea();
+		int rec2_area= rec2.calcArea();
+		System.out.println("area sum - " + ( rec1_area  + rec2_area));
+		int rec1_perimeter= rec1.calcPerimeter();
+		int rec2_perimeter= rec2.calcPerimeter();
+		System.out.println("Perimeter sum - " + ( rec1_perimeter  + rec2_perimeter));
+		
 	}
 
 }
